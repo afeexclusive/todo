c53627f85dbea962c255c169bc0dbffa2501e183
afeexclusive <afeexclusive@gmail.com>
2019-12-06 03:16:11 -0800

    Initial commit with some features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  todoArray =[];
+  addTodo(item){
+    this.todoArray.push(item);
+    document.getElementById('enttodo').value = ''
+    console.log(this.todoArray);
+  }
+
+  deleteItem(item){
+    for(let i=0; i<=this.todoArray.length; i++){
+      if (item == this.todoArray[i]){
+        this.todoArray.splice(i, 1)
+      }
+    }
+    console.log(this.todoArray);
+  }
+
+  
 }
